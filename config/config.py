f819f1b7766aca6e7075da471208909050395d47
Hari Singh <121376839+Lover-Music@users.noreply.github.com>
2023-01-11 13:01:33 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -9,7 +9,7 @@ load_dotenv()
 
 API_ID = int(getenv("API_ID", "13861846"))
 API_HASH = getenv("API_HASH", "4001710ae09a26c8326db4776b876d01")
-BOT_TOKEN = getenv("BOT_TOKEN", "5657806589:AAFU5COS7-Ao98snL_LX68ucdK_oROlT_i4")
+BOT_TOKEN = getenv("BOT_TOKEN", "5915427728:AAFk10J3SpfC9MZzhQVy2K-Q8kb2nEkAs6w")
 MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://2004:2004@cluster0.vugmi1n.mongodb.net/?retryWrites=true&w=majority")
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", "-1001898472056"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝙇𝙊𝙑𝙀𝙍 ✘ 𝗧𝗖𝗖 [𝟭]")
@@ -74,7 +74,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(
 )
 
 
-STRING1 = getenv("STRING_SESSION", "BQCzMdThQFFDQH2v2rLmmQjW2PBDAndpmUJwRsXDxPHS51ch0THau0dnoIWb4c2Fh_bW351MDDbmczZd-d8qyiMZdaCdmy1waqCu7nfFuMsTINw8DE9VNwi1sqxScJnoHWCyTUVzbA-mqs9gkUaGDvuDmCoqc7FOrm34PjIqbtxwP1dBq23msTN16A7SDrLTrp9UOgQnlqBx04Vb7ugxhsr84N3VHgPZ2Chi_FmdOv3aHfSxOViU1ihXhErN0UNDqaG-_D0IoF_zDO-HrFTVM-kZf7ixEH1QmKHnfUdWO6NAuNmckW3wjbKWlf6FNEesHhVMb5Xe0UmagaMRr46oBKrkAAAAAVKlSlsA")
+STRING1 = getenv("STRING_SESSION", "AQClIwz8b2qO394d49zkZOpqbV_tJjlwqT56jUEK-3Sb6IOpBv3TvvZkQ_-fAtF5mfxgV-4xfB8jPLOr4gnX3-a77VeXBbflr48XaWBiaRn5Ummx-hLP2UXMcs9fqgXZQa9gbSc3L2wr5jBxMazGYQ1Y1yaRZMGMgK3ZKdC3ObeWWe7LAmXC1bzgVHdm-dPDxAFGlp05gyWI7hLNzVAKAg1jtCzuefPDhpIevAYW1biVE3ZzXOSN2o9UR0ssrYp9M5aXMYsZeYPH0zoRbvIEmJNFnvQRDkOTdy1eaWonYn2G8JhmAswp-rAKACQ3HoD7jRCSe693CwqwFmPA7Qv-bi68AAAAAVB1U5UA")
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
